ce66d41c034575634297da3f39708de1f0e25d83
Stijn Nolet <stijnnolet@hotmail.com>
2020-04-20 21:54:21 +0200

    klaar met moment of inertia berekening over de hele span

--- moment_of_inertia.py
+++ moment_of_inertia.py
@@ -4,23 +4,24 @@ from parapy.core import *
 from parapy.geom import *
 from math import atan, cos, sin, tan, radians, pi
 
-mid_airfoil_test = '9412'
-tip_airfoil_test = '9408'
-spoiler_span_test = 3000.
-spoiler_chord_test = 800.
-spoiler_angle_test = 20.
-tip_cant_test = 15.
+# mid_airfoil_test = '9412'
+# tip_airfoil_test = '9408'
+# spoiler_span_test = 3000.
+# spoiler_chord_test = 800.
+# spoiler_angle_test = 20.
+# tip_cant_test = 15.
 
 
 class MomentOfInertia(GeomBase):
-    airfoil_mid = Input('9412')
-    airfoil_tip = Input('9412')
-    span = Input(3000)
-    chord = Input(800)
-    angle = Input(0)
-    tip_cant = Input(0)
-    thickness = Input(20)
-    n_discretise = Input(200)
+    airfoil_mid = Input()
+    airfoil_tip = Input()
+    span = Input()
+    chord = Input()
+    angle = Input()
+    tip_cant = Input()
+    thickness = Input()
+    n_discretise = Input(300)
+    n_cuts = Input()
 
     @Attribute
     def airfoil_names(self):
@@ -32,7 +33,7 @@ class MomentOfInertia(GeomBase):
         tip_position = self.position.translate('y', self.span / 2)
         return mid_position, tip_position
 
-    @Part(in_tree=True)
+    @Part(in_tree=False)
     def sections(self):
         return Section(quantify=2,
                        airfoil_name=self.airfoil_names[child.index],
@@ -40,84 +41,136 @@ class MomentOfInertia(GeomBase):
                        angle=self.angle,
                        position=self.section_positions[child.index])
 
+    @Part(in_tree=False)
+    def surface_lofted(self):
+        return LoftedSurface(profiles=[section.curve for section in self.sections])
+
+    @Attribute
+    def cutout_curves(self):
+        equidistant_spacing = self.span/2/(self.n_cuts-1)
+        cutout_list = []
+        for i in range(self.n_cuts):
+            cutout_list.append(self.surface_lofted.intersection_curves(
+                    other_surface=Plane(reference=translate(self.position, "y", i*equidistant_spacing), normal=self.position.Vy)))
+        return cutout_list
+
+    @Part(in_tree=False)
+    def spanwise_cutout_curves(self):
+        return ComposedCurve(quantify=self.n_cuts,
+                             built_from=[self.cutout_curves[child.index][0]])
+
     @Attribute
     def coordinates_sections_points(self):
-        crv = self.sections[0].curve
-        return crv.equispaced_points(self.n_discretise)
+        crv = []
+        for i in range(self.n_cuts):
+            crv.append(self.spanwise_cutout_curves[i].equispaced_points(self.n_discretise))
+        return crv
 
     @Attribute
     def amount_of_lines(self):
         return self.n_discretise - 1
 
-    @Part
+    @Attribute
     def lines_on_airfoil(self):
-        return LineSegment(quantify=self.amount_of_lines,
-                           start=self.coordinates_sections_points[child.index],
-                           end=self.coordinates_sections_points[child.index + 1])
-
-    # @Part
-    # def lines_on_airfoil(self):
-    #     return LineSegment(quantify=self.amount_of_lines,
-    #                        start=Point(0, 0, 0),
-    #                        end=Point(103.92, 0, 60))
+        line_segment_list = []
+        for i in range(self.n_cuts):
+            line_segment_list.append([])
+            for j in range(self.amount_of_lines):
+                line_segment_list[i].append(LineSegment(start=self.coordinates_sections_points[i][j],
+                                                        end=self.coordinates_sections_points[i][j + 1]))
+        return line_segment_list
 
     @Attribute
     def centroid_calculation(self):
-        x = 0
-        y = self.lines_on_airfoil[0].position.y
-        z = 0
-        for i in range(self.amount_of_lines):
-            x = x + (self.lines_on_airfoil[i].midpoint.x * self.lines_on_airfoil[i].length * self.thickness) \
-                / ((self.amount_of_lines) * self.lines_on_airfoil[i].length * self.thickness)
-            z = z + (self.lines_on_airfoil[i].midpoint.z * self.lines_on_airfoil[i].length * self.thickness) \
-                / ((self.amount_of_lines) * self.lines_on_airfoil[i].length * self.thickness)
-        return x, y, z
+        centroid_calculation_list = []
+        for i in range(self.n_cuts):
+            x = 0
+            y = self.lines_on_airfoil[i][0].position.y
+            z = 0
+            for j in range(self.amount_of_lines):
+                x = x + (self.lines_on_airfoil[i][j].midpoint.x * self.lines_on_airfoil[i][j].length * self.thickness) \
+                    / (self.amount_of_lines * self.lines_on_airfoil[i][j].length * self.thickness)
+                z = z + (self.lines_on_airfoil[i][j].midpoint.z * self.lines_on_airfoil[i][j].length * self.thickness) \
+                    / (self.amount_of_lines * self.lines_on_airfoil[i][j].length * self.thickness)
+            centroid_calculation_list.append([x, y, z])
+        return centroid_calculation_list
 
-    @Part
+    @Attribute
     def centroid(self):
-        return Point(self.centroid_calculation[0], self.centroid_calculation[1], self.centroid_calculation[2])
+        centroid_list = []
+        for i in range(self.n_cuts):
+            centroid_list.append(Point(self.centroid_calculation[i][0], self.centroid_calculation[i][1], self.centroid_calculation[i][2]))
+        return centroid_list
 
     @Attribute
     def moment_inertia_displaced_area(self):
-        ix_area = 0
-        iz_area = 0
-        ixz_area = 0
-        for i in range(self.amount_of_lines):
-            ix_area = ix_area + (self.lines_on_airfoil[i].length * self.thickness) \
-                      * (self.lines_on_airfoil[i].midpoint.z - self.centroid_calculation[2]) ** 2
-            iz_area = iz_area + (self.lines_on_airfoil[i].length * self.thickness) \
-                      * (self.lines_on_airfoil[i].midpoint.x - self.centroid_calculation[0]) ** 2
-            ixz_area = ixz_area + (self.lines_on_airfoil[i].length * self.thickness) \
-                      * (self.lines_on_airfoil[i].midpoint.z - self.centroid_calculation[2]) \
-                      * (self.lines_on_airfoil[i].midpoint.x - self.centroid_calculation[0])
-        return ix_area, iz_area, ixz_area
+        moment_inertia_displaced_area_list = []
+        for i in range(self.n_cuts):
+            ix_area = 0
+            iz_area = 0
+            ixz_area = 0
+            for j in range(self.amount_of_lines):
+                ix_area = ix_area + (self.lines_on_airfoil[i][j].length * self.thickness) \
+                          * (self.lines_on_airfoil[i][j].midpoint.z - self.centroid_calculation[i][2]) ** 2
+                iz_area = iz_area + (self.lines_on_airfoil[i][j].length * self.thickness) \
+                          * (self.lines_on_airfoil[i][j].midpoint.x - self.centroid_calculation[i][0]) ** 2
+                ixz_area = ixz_area + (self.lines_on_airfoil[i][j].length * self.thickness) \
+                          * (self.lines_on_airfoil[i][j].midpoint.z - self.centroid_calculation[i][2]) \
+                          * (self.lines_on_airfoil[i][j].midpoint.x - self.centroid_calculation[i][0])
+            moment_inertia_displaced_area_list.append([ix_area, iz_area, ixz_area])
+        return moment_inertia_displaced_area_list
 
     @Attribute
     def moment_inertia_area_rectangle(self):
-        ix = 0
-        iz = 0
-        ixz = 0
-        for i in range(self.amount_of_lines):
-            iu = 1 / 12 * self.lines_on_airfoil[i].length * self.thickness ** 3
-            iw = 1 / 12 * self.thickness * self.lines_on_airfoil[i].length ** 3
-            theta = radians(90) if abs(self.lines_on_airfoil[i].point1.x - self.lines_on_airfoil[i].point2.x) < (10 ** -10) \
-                else atan((self.lines_on_airfoil[i].point1.z - self.lines_on_airfoil[i].point2.z)
-                          / (self.lines_on_airfoil[i].point1.x - self.lines_on_airfoil[i].point2.x))
-            ix = ix + ((iu + iw) / 2 + (iu - iw) / 2 * cos(2 * theta))
-            iz = iz + ((iu + iw) / 2 - (iu - iw) / 2 * cos(2 * theta))
-            ixz = ixz + ((iu - iw) / 2 * sin(2 * theta))
-        return ix, iz, ixz
-    
+        moment_inertia_area_rectangle_list = []
+        for i in range(self.n_cuts):
+            ix = 0
+            iz = 0
+            ixz = 0
+            for j in range(self.amount_of_lines):
+                iu = 1 / 12 * self.lines_on_airfoil[i][j].length * self.thickness ** 3
+                iw = 1 / 12 * self.thickness * self.lines_on_airfoil[i][j].length ** 3
+                theta = radians(90) if abs(self.lines_on_airfoil[i][j].point1.x - self.lines_on_airfoil[i][j].point2.x) < (10 ** -10) \
+                    else atan((self.lines_on_airfoil[i][j].point1.z - self.lines_on_airfoil[i][j].point2.z)
+                              / (self.lines_on_airfoil[i][j].point1.x - self.lines_on_airfoil[i][j].point2.x))
+                ix = ix + ((iu + iw) / 2 + (iu - iw) / 2 * cos(2 * theta))
+                iz = iz + ((iu + iw) / 2 - (iu - iw) / 2 * cos(2 * theta))
+                ixz = ixz + ((iu - iw) / 2 * sin(2 * theta))
+            moment_inertia_area_rectangle_list.append([ix, iz, ixz])
+        return moment_inertia_area_rectangle_list
+
     @Attribute
     def moment_inertia_total(self):
-        ix_total = self.moment_inertia_displaced_area[0] + self.moment_inertia_area_rectangle[0]
-        iz_total = self.moment_inertia_displaced_area[1] + self.moment_inertia_area_rectangle[1]
-        ixz_total = self.moment_inertia_displaced_area[2] + self.moment_inertia_area_rectangle[2]
-        return ix_total, iz_total, ixz_total
+        moment_inertia_list = []
+        for i in range(self.n_cuts):
+            ix_total = self.moment_inertia_displaced_area[i][0] + self.moment_inertia_area_rectangle[i][0]
+            iz_total = self.moment_inertia_displaced_area[i][1] + self.moment_inertia_area_rectangle[i][1]
+            ixz_total = self.moment_inertia_displaced_area[i][2] + self.moment_inertia_area_rectangle[i][2]
+            moment_inertia_list.append([ix_total, iz_total, ixz_total])
+        return moment_inertia_list
+
+    @Attribute
+    def full_moment_of_inertia(self):
+        full_moment_inertia_list = []
+        for i in range(self.n_cuts*2-1):
+            if i < self.n_cuts:
+                full_moment_inertia_list.append(self.moment_inertia_total[self.n_cuts - i - 1])
+            else:
+                full_moment_inertia_list.append(self.moment_inertia_total[i - self.n_cuts + 1])
+        return full_moment_inertia_list
 
 
 if __name__ == '__main__':
     from parapy.gui import display
-
-    obj = MomentOfInertia(label='points')
+    obj = MomentOfInertia(label='points',
+                          airfoil_mid='9412',
+                          airfoil_tip='9408',
+                          span=3000.,
+                          chord=800.,
+                          angle=0.,
+                          tip_cant=0.,
+                          thickness=4.,
+                          n_discretise=300,
+                          n_cuts=5)
     display(obj)
+
