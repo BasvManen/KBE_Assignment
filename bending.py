1bca745bee0aa14befa69f0726ca051b66f13342
Stijn Nolet <stijnnolet@hotmail.com>
2020-04-21 16:54:35 +0200

    bending gekoppeld aan moment of inertia

--- bending.py
+++ bending.py
@@ -1,9 +1,13 @@
 from math import pi, sin, cos, tan, radians
+from moment_of_inertia import MomentOfInertia
+from parapy.geom import *
+from parapy.core import *
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-# function to calculate moment due to a distributed force
+# function to calculate moment due to a distributed force (list1),
+# for a spatial list (y_i) set at some spatial value (elementset)
 def sumz_moment(list1, y_i, elementset):
     list2 = []
     for i in range(len(list1)):
@@ -15,33 +19,36 @@ def sumz_moment(list1, y_i, elementset):
     return sum(list2)
 
 
-# function to calculate stresses/displacement of the main spoiler plate
-def mainplate_bending(lift, E, Ixx, spoiler_weight, endplate_weight, spoiler_span, spoiler_chord, spoiler_area, strut_lat_location):
+# function to calculate moments/displacement of the main spoiler plate using as inputs the aerodynamic force on the
+# spoiler, the material and sectional properties of the spoiler and the geometric properties of the spoiler design
+def mainplate_bending(lift, E, Ixx, spoiler_weight, endplate_weight, spoiler_span, spoiler_chord, spoiler_area,
+                      strut_lat_location):
+    # retrieve y-location of the struts
     strut_location_1 = spoiler_span / 2 * (1 - strut_lat_location)
     strut_location_2 = spoiler_span / 2 * (1 + strut_lat_location)
-    
+
     # calculating y-coordinate, area and weight at each increment i
     y_i = np.zeros(len(lift) + 1)
     y_ii = np.zeros(len(lift))
     area_i = np.zeros(len(lift))
     weight_i = np.zeros(len(lift))
     di = spoiler_span / len(lift)
-    
+
     for i in range(len(lift) + 1):
         y_i[i] = round(i * di, 3)
-    
+
     for i in range(len(lift)):
         y_ii[i] = y_i[i] + (y_i[i + 1] - y_i[i]) / 2
         area_i[i] = spoiler_chord * di
         weight_i[i] = -spoiler_weight / spoiler_area * area_i[i]  # force downward
-    
+
     # adding the weight of the endplate to the first and last weight_i
     weight_i[0] += endplate_weight
     weight_i[-1] += endplate_weight
-    
+
     # calculate the z-force on the strut by sum of forces in z
     f_strut_z = -(sum(lift) + sum(weight_i)) / 2
-    
+
     # calculate the influence of the lift and weight on the bending moment for each increment i
     moment_lift_i = []
     moment_weight_i = []
@@ -51,13 +58,14 @@ def mainplate_bending(lift, E, Ixx, spoiler_weight, endplate_weight, spoiler_spa
         momentW = sumz_moment(weight_i, y_ii, yset)
         moment_lift_i.append(momentL)
         moment_weight_i.append(momentW)
-    
-    # calculating the moment along strut in x
+
+    # calculating the moment in x along the spoiler
     moment_x_i = []
     for i in range(len(lift) + 1):
         yset = y_i[i]
         if yset >= strut_location_2 and yset >= strut_location_1:
-            mom_i = f_strut_z * (yset - strut_location_2) + f_strut_z * (yset - strut_location_1) + moment_lift_i[i] + moment_weight_i[i]
+            mom_i = f_strut_z * (yset - strut_location_2) + f_strut_z * (yset - strut_location_1) + \
+                    moment_lift_i[i] + moment_weight_i[i]
             moment_x_i.append(mom_i)
         elif strut_location_2 > yset >= strut_location_1:
             mom_i = f_strut_z * (yset - strut_location_1) + moment_lift_i[i] + moment_weight_i[i]
@@ -65,54 +73,99 @@ def mainplate_bending(lift, E, Ixx, spoiler_weight, endplate_weight, spoiler_spa
         elif yset < strut_location_2 and yset < strut_location_1:
             mom_i = moment_lift_i[i] + moment_weight_i[i]
             moment_x_i.append(mom_i)
-    
-    # calculate deflection angles and displacement
+
+    # calculate deflection angles and displacement using Euler-Bernoulli beam theory
     # the deflection angle in the centerline of the spoiler is 0
     # the deflection at the struts is considered equal to 0
     theta_i = np.zeros(len(lift) + 1)
     for i in range(int(len(lift) / 2) + 1, len(lift) + 1):
-        theta_i[i] = theta_i[i - 1] + 0.5 * (moment_x_i[i] / (E * Ixx[i]) + moment_x_i[i - 1] / (E * Ixx[i - 1])) * (
-                    y_i[i] - y_i[i - 1])
-    
+        theta_i[i] = theta_i[i - 1] + 0.5 * (
+                moment_x_i[i] / (E * Ixx[i]) + moment_x_i[i - 1] / (E * Ixx[i - 1])) * (
+                             y_i[i] - y_i[i - 1])
+
     index_strut = np.where(y_i == strut_location_2)[0][0]
     v_i = np.zeros(len(lift) + 1)
     for i in range(index_strut, len(lift) + 1):
         v_i[i] = v_i[i - 1] + 0.5 * (theta_i[i] + theta_i[i - 1]) * (y_i[i] - y_i[i - 1])
-    
+
     for i in range(index_strut + 1, int(len(lift) / 2), -1):
         v_i[i - 1] = v_i[i] - 0.5 * (theta_i[i] + theta_i[i - 1]) * (y_i[i] - y_i[i - 1])
-    
+
     for i in range(int(len(lift) / 2)):
         theta_i[i] = theta_i[len(lift) - i]
         v_i[i] = v_i[len(lift) - i]
-        
+
     return theta_i, v_i, y_i, moment_x_i
 
 
-# input parameters of the geometry
-g = 9.80665
-spoiler_weight = 20 * g
-endplate_weight = 0.5 * g
-spoiler_span = 2
-spoiler_chord = 0.8
-spoiler_area = 2.4
-strut_lat_location = 0.9
+class Bending(GeomBase):
 
-# list with the lift force at each increment i
-lift = -100 * np.ones(200)
+    # Inputs for moment of inertia calculations
+    airfoil_mid = Input()
+    airfoil_tip = Input()
+    spoiler_span = Input()
+    spoiler_chord = Input()
+    spoiler_angle = Input()
+    spoiler_skin_thickness = Input()
 
-# input sectional parameters
-E = 70 * 10 ** 9
-Ixx = 10 ** -6 * np.ones(len(lift) + 1)
+    # Additional inputs for bending calculations
+    spoiler_weight = Input()
+    endplate_weight = Input()
+    spoiler_area = Input()
+    strut_lat_location = Input()
+    lift = Input()
+    E = Input()
 
-theta_i, v_i, y_i, moment_x_i = mainplate_bending(lift, E, Ixx, spoiler_weight, endplate_weight, spoiler_span, spoiler_chord, spoiler_area, strut_lat_location)
+    @Attribute
+    def number_of_lateral_cuts(self):
+        return int(len(self.lift)/2) + 1
 
-# plotting
-plt.figure()
-plt.plot(y_i, theta_i)
-plt.plot(y_i, v_i)
-plt.grid()
+    @Attribute
+    def full_moment_of_inertia(self):
+        return MomentOfInertia(airfoil_mid=self.airfoil_mid,
+                               airfoil_tip=self.airfoil_tip,
+                               spoiler_span=self.spoiler_span,
+                               spoiler_chord=self.spoiler_chord,
+                               spoiler_angle=self.spoiler_angle,
+                               spoiler_skin_thickness=self.spoiler_skin_thickness,
+                               n_cuts=self.number_of_lateral_cuts).full_moment_of_inertia
 
-plt.figure()
-plt.plot(y_i, moment_x_i)
-plt.grid()
+    @Attribute
+    def x_moment_of_inertia(self):
+        return [row[0] for row in self.full_moment_of_inertia]
+
+    @Attribute
+    def bending_output(self):
+        theta_i, w_i, y_i, moment_x_i = mainplate_bending(self.lift, self.E, self.x_moment_of_inertia,
+                                                          self.spoiler_weight, self.endplate_weight, self.spoiler_span,
+                                                          self.spoiler_chord, self.spoiler_area,
+                                                          self.strut_lat_location)
+        return theta_i, w_i, y_i, moment_x_i
+
+    @Attribute
+    def plot_w_deflection(self):
+        plt.plot(self.bending_output[2], self.bending_output[3])
+        plt.xlabel('Spanwise location [m]')
+        plt.ylabel('Deflection in z [m]')
+        plt.grid(b=True, which='both', color='0.65', linestyle='-')
+        plt.title("Close it to refresh the ParaPy GUI")
+        plt.show()
+        return "Plot generated and closed"
+
+
+if __name__ == '__main__':
+    from parapy.gui import display
+    obj = Bending(label="Spoiler",
+                  airfoil_mid='9412',
+                  airfoil_tip='9408',
+                  spoiler_span=3.,
+                  spoiler_chord=0.8,
+                  spoiler_angle=20.,
+                  spoiler_skin_thickness=0.002,
+                  spoiler_weight=200,
+                  endplate_weight=50,
+                  spoiler_area=2.4,
+                  strut_lat_location=0.7,
+                  lift=-100*np.ones(40),
+                  E=6.89*10**7)
+    display(obj)
--- moment_of_inertia.py
+++ moment_of_inertia.py
@@ -6,20 +6,18 @@ from math import atan, cos, sin, tan, radians, pi
 
 # mid_airfoil_test = '9412'
 # tip_airfoil_test = '9408'
-# spoiler_span_test = 3000.
-# spoiler_chord_test = 800.
-# spoiler_angle_test = 20.
-# tip_cant_test = 15.
+# spoiler_spoiler_span_test = 3000.
+# spoiler_spoiler_chord_test = 800.
+# spoiler_spoiler_angle_test = 20.
 
 
 class MomentOfInertia(GeomBase):
     airfoil_mid = Input()
     airfoil_tip = Input()
-    span = Input()
-    chord = Input()
-    angle = Input()
-    tip_cant = Input()
-    thickness = Input()
+    spoiler_span = Input()
+    spoiler_chord = Input()
+    spoiler_angle = Input()
+    spoiler_skin_thickness = Input()
     n_discretise = Input(300)
     n_cuts = Input()
 
@@ -30,15 +28,15 @@ class MomentOfInertia(GeomBase):
     @Attribute
     def section_positions(self):
         mid_position = self.position
-        tip_position = self.position.translate('y', self.span / 2)
+        tip_position = self.position.translate('y', self.spoiler_span / 2)
         return mid_position, tip_position
 
     @Part(in_tree=False)
     def sections(self):
         return Section(quantify=2,
                        airfoil_name=self.airfoil_names[child.index],
-                       chord=self.chord,
-                       angle=self.angle,
+                       chord=self.spoiler_chord,
+                       angle=self.spoiler_angle,
                        position=self.section_positions[child.index])
 
     @Part(in_tree=False)
@@ -47,7 +45,7 @@ class MomentOfInertia(GeomBase):
 
     @Attribute
     def cutout_curves(self):
-        equidistant_spacing = self.span/2/(self.n_cuts-1)
+        equidistant_spacing = self.spoiler_span/2/(self.n_cuts-1)
         cutout_list = []
         for i in range(self.n_cuts):
             cutout_list.append(self.surface_lofted.intersection_curves(
@@ -55,7 +53,7 @@ class MomentOfInertia(GeomBase):
         return cutout_list
 
     @Part(in_tree=False)
-    def spanwise_cutout_curves(self):
+    def spoiler_spanwise_cutout_curves(self):
         return ComposedCurve(quantify=self.n_cuts,
                              built_from=[self.cutout_curves[child.index][0]])
 
@@ -63,7 +61,7 @@ class MomentOfInertia(GeomBase):
     def coordinates_sections_points(self):
         crv = []
         for i in range(self.n_cuts):
-            crv.append(self.spanwise_cutout_curves[i].equispaced_points(self.n_discretise))
+            crv.append(self.spoiler_spanwise_cutout_curves[i].equispaced_points(self.n_discretise))
         return crv
 
     @Attribute
@@ -88,10 +86,10 @@ class MomentOfInertia(GeomBase):
             y = self.lines_on_airfoil[i][0].position.y
             z = 0
             for j in range(self.amount_of_lines):
-                x = x + (self.lines_on_airfoil[i][j].midpoint.x * self.lines_on_airfoil[i][j].length * self.thickness) \
-                    / (self.amount_of_lines * self.lines_on_airfoil[i][j].length * self.thickness)
-                z = z + (self.lines_on_airfoil[i][j].midpoint.z * self.lines_on_airfoil[i][j].length * self.thickness) \
-                    / (self.amount_of_lines * self.lines_on_airfoil[i][j].length * self.thickness)
+                x = x + (self.lines_on_airfoil[i][j].midpoint.x * self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness) \
+                    / (self.amount_of_lines * self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness)
+                z = z + (self.lines_on_airfoil[i][j].midpoint.z * self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness) \
+                    / (self.amount_of_lines * self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness)
             centroid_calculation_list.append([x, y, z])
         return centroid_calculation_list
 
@@ -110,42 +108,42 @@ class MomentOfInertia(GeomBase):
             iz_area = 0
             ixz_area = 0
             for j in range(self.amount_of_lines):
-                ix_area = ix_area + (self.lines_on_airfoil[i][j].length * self.thickness) \
+                ix_area = ix_area + (self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness) \
                           * (self.lines_on_airfoil[i][j].midpoint.z - self.centroid_calculation[i][2]) ** 2
-                iz_area = iz_area + (self.lines_on_airfoil[i][j].length * self.thickness) \
+                iz_area = iz_area + (self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness) \
                           * (self.lines_on_airfoil[i][j].midpoint.x - self.centroid_calculation[i][0]) ** 2
-                ixz_area = ixz_area + (self.lines_on_airfoil[i][j].length * self.thickness) \
+                ixz_area = ixz_area + (self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness) \
                           * (self.lines_on_airfoil[i][j].midpoint.z - self.centroid_calculation[i][2]) \
                           * (self.lines_on_airfoil[i][j].midpoint.x - self.centroid_calculation[i][0])
             moment_inertia_displaced_area_list.append([ix_area, iz_area, ixz_area])
         return moment_inertia_displaced_area_list
 
     @Attribute
-    def moment_inertia_area_rectangle(self):
-        moment_inertia_area_rectangle_list = []
+    def moment_inertia_area_rectspoiler_angle(self):
+        moment_inertia_area_rectspoiler_angle_list = []
         for i in range(self.n_cuts):
             ix = 0
             iz = 0
             ixz = 0
             for j in range(self.amount_of_lines):
-                iu = 1 / 12 * self.lines_on_airfoil[i][j].length * self.thickness ** 3
-                iw = 1 / 12 * self.thickness * self.lines_on_airfoil[i][j].length ** 3
+                iu = 1 / 12 * self.lines_on_airfoil[i][j].length * self.spoiler_skin_thickness ** 3
+                iw = 1 / 12 * self.spoiler_skin_thickness * self.lines_on_airfoil[i][j].length ** 3
                 theta = radians(90) if abs(self.lines_on_airfoil[i][j].point1.x - self.lines_on_airfoil[i][j].point2.x) < (10 ** -10) \
                     else atan((self.lines_on_airfoil[i][j].point1.z - self.lines_on_airfoil[i][j].point2.z)
                               / (self.lines_on_airfoil[i][j].point1.x - self.lines_on_airfoil[i][j].point2.x))
                 ix = ix + ((iu + iw) / 2 + (iu - iw) / 2 * cos(2 * theta))
                 iz = iz + ((iu + iw) / 2 - (iu - iw) / 2 * cos(2 * theta))
                 ixz = ixz + ((iu - iw) / 2 * sin(2 * theta))
-            moment_inertia_area_rectangle_list.append([ix, iz, ixz])
-        return moment_inertia_area_rectangle_list
+            moment_inertia_area_rectspoiler_angle_list.append([ix, iz, ixz])
+        return moment_inertia_area_rectspoiler_angle_list
 
     @Attribute
     def moment_inertia_total(self):
         moment_inertia_list = []
         for i in range(self.n_cuts):
-            ix_total = self.moment_inertia_displaced_area[i][0] + self.moment_inertia_area_rectangle[i][0]
-            iz_total = self.moment_inertia_displaced_area[i][1] + self.moment_inertia_area_rectangle[i][1]
-            ixz_total = self.moment_inertia_displaced_area[i][2] + self.moment_inertia_area_rectangle[i][2]
+            ix_total = self.moment_inertia_displaced_area[i][0] + self.moment_inertia_area_rectspoiler_angle[i][0]
+            iz_total = self.moment_inertia_displaced_area[i][1] + self.moment_inertia_area_rectspoiler_angle[i][1]
+            ixz_total = self.moment_inertia_displaced_area[i][2] + self.moment_inertia_area_rectspoiler_angle[i][2]
             moment_inertia_list.append([ix_total, iz_total, ixz_total])
         return moment_inertia_list
 
@@ -162,14 +160,13 @@ class MomentOfInertia(GeomBase):
 
 if __name__ == '__main__':
     from parapy.gui import display
-    obj = MomentOfInertia(label='points',
+    obj = MomentOfInertia(label='Moment of Inertia',
                           airfoil_mid='9412',
                           airfoil_tip='9408',
-                          span=3000.,
-                          chord=800.,
-                          angle=0.,
-                          tip_cant=0.,
-                          thickness=4.,
+                          spoiler_span=3000.,
+                          spoiler_chord=800.,
+                          spoiler_angle=0.,
+                          spoiler_skin_thickness=4.,
                           n_discretise=300,
                           n_cuts=5)
     display(obj)
