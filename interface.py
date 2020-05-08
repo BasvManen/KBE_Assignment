0b1e1060bb3b9b438dec92f81a78cea1b50e05f8
Bas <BasvManen@Gmail.com>
2020-05-08 20:06:52 +0200

    File name gefixt

--- interface.py
+++ interface.py
@@ -16,29 +16,51 @@ print("")
 
 var_input = int(input("Input: "))
 
-os.system('cls')
-
-# print("Please provide the name of the geometry input file")
-# print("")
-# geom_input = str(input("Input: "))
+# os.system('cls')
 #
-# geom_input = 'inputs/' + geom_input
+# check = 0
+# while check == 0:
+#     print("Please provide the name of the geometry input file")
+#     print("")
+#     geom_input = str(input("Input: "))
+#     geom_input = 'inputs/' + geom_input
+#
+#     if os.path.exists(geom_input):
+#         check = 1
+#     else:
+#         print("Please provide a valid file name")
+#         print("")
+#
 # geom = read_geometry_inputs(geom_input)
 #
-# print("")
-# print("Please provide the name of the flow conditions input file")
-# print("")
-# cond_input = str(input("Input: "))
+# check = 0
+# while check == 0:
+#     print("")
+#     print("Please provide the name of the flow conditions input file")
+#     print("")
+#     cond_input = str(input("Input: "))
+#     cond_input = 'inputs/' + cond_input
 #
-# cond_input = 'inputs/' + cond_input
+#     if os.path.exists(cond_input):
+#         check = 1
+#     else:
+#         print("Please provide a valid file name")
+#
 # cond = read_flow_inputs(cond_input)
 #
-# print("")
-# print("Please provide the name of the material property input file")
-# print("")
-# mat_input = str(input("Input: "))
+# check = 0
+# while check == 0:
+#     print("")
+#     print("Please provide the name of the material property input file")
+#     print("")
+#     mat_input = str(input("Input: "))
+#     mat_input = 'inputs/' + mat_input
 #
-# mat_input = 'inputs/' + mat_input
+#     if os.path.exists(mat_input):
+#         check = 1
+#     else:
+#         print("Please provide a valid file name")
+#
 # mat = read_material_inputs(mat_input)
 
 os.system('cls')
@@ -54,55 +76,101 @@ if var_input == 1:
 
     os.system('cls')
     if mode_input == 1:
-        print("Please provide the name of the geometry input file")
-        print("")
-        geom_input = str(input("Input: "))
+        check = 0
+        while check == 0:
+            print("Please provide the name of the geometry input file")
+            print("")
+            geom_input = str(input("Input: "))
+            geom_input = 'inputs/' + geom_input
 
-        geom_input = 'inputs/' + geom_input
+            if os.path.exists(geom_input):
+                check = 1
+            else:
+                print("Please provide a valid file name")
+                print("")
+
         geom = read_geometry_inputs(geom_input)
 
         geometry(geom)
 
     elif mode_input == 2:
-        print("Please provide the name of the geometry input file")
-        print("")
-        geom_input = str(input("Input: "))
+        check = 0
+        while check == 0:
+            print("Please provide the name of the geometry input file")
+            print("")
+            geom_input = str(input("Input: "))
+            geom_input = 'inputs/' + geom_input
 
-        geom_input = 'inputs/' + geom_input
+            if os.path.exists(geom_input):
+                check = 1
+            else:
+                print("Please provide a valid file name")
+                print("")
+
         geom = read_geometry_inputs(geom_input)
 
-        print("")
-        print("Please provide the name of the flow conditions input file")
-        print("")
-        cond_input = str(input("Input: "))
+        check = 0
+        while check == 0:
+            print("")
+            print("Please provide the name of the flow conditions input file")
+            print("")
+            cond_input = str(input("Input: "))
+            cond_input = 'inputs/' + cond_input
 
-        cond_input = 'inputs/' + cond_input
+            if os.path.exists(cond_input):
+                check = 1
+            else:
+                print("Please provide a valid file name")
+
         cond = read_flow_inputs(cond_input)
 
         avl_main(geom, cond)
 
     elif mode_input == 3:
-        print("Please provide the name of the geometry input file")
-        print("")
-        geom_input = str(input("Input: "))
+        check = 0
+        while check == 0:
+            print("Please provide the name of the geometry input file")
+            print("")
+            geom_input = str(input("Input: "))
+            geom_input = 'inputs/' + geom_input
 
-        geom_input = 'inputs/' + geom_input
+            if os.path.exists(geom_input):
+                check = 1
+            else:
+                print("Please provide a valid file name")
+                print("")
+
         geom = read_geometry_inputs(geom_input)
 
-        print("")
-        print("Please provide the name of the flow conditions input file")
-        print("")
-        cond_input = str(input("Input: "))
+        check = 0
+        while check == 0:
+            print("")
+            print("Please provide the name of the flow conditions input file")
+            print("")
+            cond_input = str(input("Input: "))
+            cond_input = 'inputs/' + cond_input
 
-        cond_input = 'inputs/' + cond_input
+            if os.path.exists(cond_input):
+                check = 1
+            else:
+                print("Please provide a valid file name")
+
         cond = read_flow_inputs(cond_input)
 
-        print("")
-        print("Please provide the name of the material property input file")
-        print("")
-        mat_input = str(input("Input: "))
+        check = 0
+        while check == 0:
+            print("")
+            print(
+                "Please provide the name of the material property input file")
+            print("")
+            mat_input = str(input("Input: "))
+            mat_input = 'inputs/' + mat_input
 
-        mat_input = 'inputs/' + mat_input
+            if os.path.exists(mat_input):
+                check = 1
+            else:
+                print("Please provide a valid file name")
+
         mat = read_material_inputs(mat_input)
 
         print("Please enter an initial skin thickness in meters")
@@ -112,19 +180,34 @@ if var_input == 1:
                             initial_skin_thickness=initial_thickness)
 
 elif var_input == 2:
-    print("Please provide the name of the geometry input file")
-    print("")
-    geom_input = str(input("Input: "))
+    check = 0
+    while check == 0:
+        print("Please provide the name of the geometry input file")
+        print("")
+        geom_input = str(input("Input: "))
+        geom_input = 'inputs/' + geom_input
 
-    geom_input = 'inputs/' + geom_input
+        if os.path.exists(geom_input):
+            check = 1
+        else:
+            print("Please provide a valid file name")
+            print("")
+
     geom = read_geometry_inputs(geom_input)
 
-    print("")
-    print("Please provide the name of the flow conditions input file")
-    print("")
-    cond_input = str(input("Input: "))
+    check = 0
+    while check == 0:
+        print("")
+        print("Please provide the name of the flow conditions input file")
+        print("")
+        cond_input = str(input("Input: "))
+        cond_input = 'inputs/' + cond_input
 
-    cond_input = 'inputs/' + cond_input
+        if os.path.exists(cond_input):
+            check = 1
+        else:
+            print("Please provide a valid file name")
+
     cond = read_flow_inputs(cond_input)
 
     geometry_calculator(geom, cond)
