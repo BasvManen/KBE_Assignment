5411bcc23cfe450838cd7d67cba36ef1a8c9f14d
Stijn Nolet <stijnnolet@hotmail.com>
2020-05-28 11:35:02 +0200

    comments in sectional_properties

--- analysis/section_properties.py
+++ analysis/section_properties.py
@@ -4,7 +4,28 @@ from parapy.geom import *
 from math import atan, cos, sin, radians
 
 
+###############################################################################
+# SECTIONAL PROPERTIES CLASS                                                  #
+# In this file, several sectional properties along the spoiler are calculated #
+#                                                                             #
+# Inputs:                                                                     #
+# - airfoils, defined in a list of strings. See documentation of the Section  #
+#   class for profile definition.                                             #
+# - Span of the main plate                                                    #
+# - Chord of the main plate                                                   #
+# - Angle of the main plate, positive defined upwards                         #
+# - Skin thickness of the spoiler for the structural calculations             #
+# - Number of ribs in the spoiler main plate                                  #
+# - n_cuts, defined as the amount of span-wise cuts are made along the        #
+#   y-direction of the main plate.                                            #
+# - n_discretise, which is the number of equidistant cuts along each of the   #
+#   cross sectional airfoil cutouts of the main plate. Optionally,            #
+#   n_discretise can be set higher in order to get a more accurate            #
+#   approximation of several of the sectional properties.                     #
+###############################################################################
+
 class SectionProperties(GeomBase):
+
     # Main plate inputs
     airfoils = Input()
     spoiler_span = Input()
@@ -14,15 +35,15 @@ class SectionProperties(GeomBase):
     n_ribs = Input()
 
     # Inputs for the discretisation of the spoiler sections
-    n_discretise = Input(120)
     n_cuts = Input()
+    n_discretise = Input(120)
 
     @Part(in_tree=True)
     def sections(self):
-        """ Create the sections based on the airfoils list input. They are
-        translated such that the first section is at the mid and the last
-        section is at the tip of the main plate. The other sections are placed
-        equidistant from mid to tip. """
+        """ As similarly in MainPlate, create the sections based on the
+        airfoils list input. They are translated such that the first section
+        is at the mid and the last section is at the tip of the main plate.
+        The other sections are placed equidistant from mid to tip. """
         return Section(quantify=len(self.airfoils),
                        airfoil_name=self.airfoils[child.index],
                        chord=self.spoiler_chord,
@@ -55,6 +76,8 @@ class SectionProperties(GeomBase):
 
     @Attribute
     def cutout_curves(self):
+        """ This attribute returns a list of equidistantly distanced cross
+        section cutouts, in the span-wise direction. """
         spacing = self.spoiler_span / 2 / (self.n_cuts - 1)
         cutout_list = []
         for i in range(self.n_cuts):
@@ -66,25 +89,32 @@ class SectionProperties(GeomBase):
 
     @Part(in_tree=False)
     def spoiler_spanwise_cutout_curves(self):
+        """ The cutout_curves list is here used to create several curve
+        parts, these parts are used for several sectional property
+        calculations. """
         return ComposedCurve(quantify=self.n_cuts,
                              built_from=[self.cutout_curves[child.index][0]])
 
-    # Retrieve the coordinates along the curve-cutouts
     @Attribute
     def coordinates_sections_points(self):
+        """ This attribute retrieves the equidistant coordinates on each of
+        the curve parts. """
         crv = []
         for i in range(self.n_cuts):
             crv.append(self.spoiler_spanwise_cutout_curves[i].
                        equispaced_points(self.n_discretise))
         return crv
 
-    # Discretise the cutouts into several small straight lines.
     @Attribute
     def amount_of_lines(self):
+        """ This attribute returns the amount of needed discretised straight
+        lines on each of the cutout curves. """
         return self.n_discretise - 1
 
     @Attribute
     def lines_on_airfoil(self):
+        """ This attribute creates small linear lines between each of the
+        coordinate points of the cutout curves along the spoiler. """
         line_segment_list = []
         for i in range(self.n_cuts):
             line_segment_list.append([])
@@ -95,9 +125,10 @@ class SectionProperties(GeomBase):
                                     j + 1]))
         return line_segment_list
 
-    # Calculate the centroid position along the spoiler
     @Attribute
     def centroid_calculation(self):
+        """ This attribute calculates the centroid's position (x, y and z
+        coordinate) of each of the cutout curves along the spoiler. """
         centroid_calculation_list = []
         for i in range(self.n_cuts):
             x = 0
@@ -121,6 +152,8 @@ class SectionProperties(GeomBase):
 
     @Attribute
     def centroid(self):
+        """ This attribute creates Point instances for each of the centroids
+        along the spoiler. """
         centroid_list = []
         for i in range(self.n_cuts):
             centroid_list.append(Point(self.centroid_calculation[i][0],
@@ -128,10 +161,10 @@ class SectionProperties(GeomBase):
                                        self.centroid_calculation[i][2]))
         return centroid_list
 
-    # Calculate the moment of inertia due to the displaced area with respect
-    # to the centroid along the spoiler
     @Attribute
     def area_along_spoiler(self):
+        """ This attribute calculates the total cross sectional area of each
+        of the cutouts along the spoiler. """
         area_list = []
         for i in range(self.n_cuts):
             area_cut = []
@@ -143,6 +176,11 @@ class SectionProperties(GeomBase):
 
     @Attribute
     def moment_inertia_displaced_area(self):
+        """ This attribute calculates the first part (due to displaced area
+        theorem) of the moment of inertia of each of the cutouts. It
+        calculates the area of each of the line segments on the cutouts and
+        multiplies it with the square of the distance between the line
+        segment and the centroid of that cutout. """
         moment_inertia_displaced_area_list = []
         for i in range(self.n_cuts):
             ix_area = 0
@@ -168,10 +206,12 @@ class SectionProperties(GeomBase):
                                                        ixz_area])
         return moment_inertia_displaced_area_list
 
-    # Calculate the moment of inertia due to the rectangular properties of
-    # the discretised lines
     @Attribute
     def moment_inertia_area_rectangle(self):
+        """ This attribute calculates the second part (due to geometric
+        properties) of the moment of inertia of each of the cutouts. It
+        calculates for each of the (angled) line segments along the cutout,
+        the moments of inertia of a rectangle. """
         moment_inertia_area_rectangle_list = []
         for i in range(self.n_cuts):
             ix = 0
@@ -195,9 +235,10 @@ class SectionProperties(GeomBase):
             moment_inertia_area_rectangle_list.append([ix, iz, ixz])
         return moment_inertia_area_rectangle_list
 
-    # Calculate the total moments of inertia (Ixx, Izz, Ixz)
     @Attribute
     def moment_inertia_total(self):
+        """ This attribute calculates the total moments of inertia (Ixx,
+        Izz, Ixz) from the displaced area and geometric attributes above. """
         moment_inertia_list = []
         for i in range(self.n_cuts):
             ix_total = self.moment_inertia_displaced_area[i][0] + \
@@ -209,12 +250,12 @@ class SectionProperties(GeomBase):
             moment_inertia_list.append([ix_total, iz_total, ixz_total])
         return moment_inertia_list
 
-    # As the above calculations are for the half-spoiler only (since the
-    # mainplate is first projected as one half and then mirrored),
-    # this function makes for the moment of inertia along the whole spoiler
-    # span.
     @Attribute
     def full_moment_of_inertia(self):
+        """ As the above calculations are for the half-spoiler only (since the
+        main plate is first only projected as one half and later mirrored),
+        this attribute returns the moments of inertia along the whole spoiler
+        span."""
         full_moment_inertia_list = []
         for i in range(self.n_cuts * 2 - 1):
             if i < self.n_cuts:
@@ -225,14 +266,20 @@ class SectionProperties(GeomBase):
                     self.moment_inertia_total[i - self.n_cuts + 1])
         return full_moment_inertia_list
 
-    # Calculate the placement of the ribs to calculate their weights
     @Attribute
     def position_ribs(self):
+        """ This attribute calculates the position of the ribs along the
+        half-span of the main plate. It first calculates the exact
+        y-position, after which the rib curves are associated with the
+        nearest discretised cutout curve, in term of their indices. """
+        # total amount of ribs including spoiler tips
         n_ribs_calc = self.n_ribs + 2
         spacing_ribs = self.spoiler_span / (n_ribs_calc - 1)
+        # calculate the exact y location of the ribs
         position_ribs = []
         for i in range(n_ribs_calc):
             position_ribs.append(i * spacing_ribs)
+        # assign the ribs to the nearest discretised cutout curve
         spacing_discretisation = self.spoiler_span / 2 / (self.n_cuts - 1)
         ribs_indices = []
         for i in range(len(position_ribs)):
@@ -242,16 +289,30 @@ class SectionProperties(GeomBase):
                     ribs_indices.append(j)
         return ribs_indices
 
-    # Make rib curves
     @Part(in_tree=False)
     def rib_curves(self):
+        """ This part creates the rib curves from the position_ribs
+        attribute. """
         return ComposedCurve(quantify=round((self.n_ribs + 2) / 2 + 0.1),
                              built_from=[self.cutout_curves[
                                              self.position_ribs[child.index]]
                                          [0]])
 
+    @Attribute
+    def ribs_area(self):
+        """ This attribute calculates the cross sectional area of each of
+        the ribs. """
+        ribs_area_list = []
+        for i in range(round((self.n_ribs + 2) / 2)):
+            ribs_area_list.append(Face(self.rib_curves[i]).area)
+        ribs_area_list = ribs_area_list[::-1] + ribs_area_list[1:]
+        return ribs_area_list
+
     @Part(in_tree=False)
     def ribs_right(self):
+        """ This part creates all ribs (with the same thickness as the
+        skin_thickness) on one side of the spoiler. Later this can be used
+        to mirror the ribs to the other side of the spoiler. """
         return RuledSolid(quantify=round((self.n_ribs + 2) / 2 + 0.1),
                           profile1=self.rib_curves[child.index],
                           profile2=
@@ -264,6 +325,9 @@ class SectionProperties(GeomBase):
 
     @Part(in_tree=False)
     def ribs_left(self):
+        """ Here ribs_right are mirrored in the center plane of the spoiler
+        (except the rib which located in the center plane of the
+        main plate in case of an uneven rib total). """
         return MirroredShape(quantify=round((self.n_ribs + 2) / 2 + 0.1)
                              if self.n_ribs % 2 == 0
                              else round((self.n_ribs + 2) / 2 + 0.1) - 1,
@@ -276,20 +340,15 @@ class SectionProperties(GeomBase):
 
     @Part(in_tree=True)
     def ribs_total(self):
+        """ This part fuses the ribs_right and ribs_left parts together,
+        to create one instance of rib parts. """
         return SewnSolid(quantify=self.n_ribs + 2,
                          built_from=self.ribs_right[child.index]
                          if child.index <= round((self.n_ribs + 2) / 2 + 0.1)-1
                          else self.ribs_left[
                              child.index - round((self.n_ribs + 2) / 2 + 0.1)])
 
-    # Calculate the area of the ribs
-    @Attribute
-    def ribs_area(self):
-        ribs_area_list = []
-        for i in range(round((self.n_ribs + 2) / 2)):
-            ribs_area_list.append(Face(self.rib_curves[i]).area)
-        ribs_area_list = ribs_area_list[::-1] + ribs_area_list[1:]
-        return ribs_area_list
+
 
 
 if __name__ == '__main__':
